deeecf7d15071b49c014024733e834d828be4816
Raquel_Rodrigues <raquel.rodrigues@amdocs.com>
2021-05-06 21:28:10 -0300

    [CHAPTER IV - Autenticação e autorização] - Autenticação com SSR

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import { FormEvent, useContext, useState } from "react"
+
 import { AuthContext } from "../contexts/AuthContext";
+import { withSSRGuest } from "../utils/withSSRGuest";
 
 export default function Home() {
   const [email, setEmail] = useState('');
@@ -27,3 +29,9 @@ export default function Home() {
     </form>
   );
 }
+
+export const getServerSideProps = withSSRGuest(async (ctx) => {
+  return {
+    props: {},
+  }
+});
